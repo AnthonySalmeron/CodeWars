41fec9d0f02f9e48299e5675dbec8b266655c4fd
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-02-21 17:44:14 -0500

    2/21/20

---
+++ codewars/arrayPartition.js
@@ -0,0 +1,16 @@
+// Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+//
+// Example 1:
+//
+// Input: [1,4,3,2]
+//
+// Output: 4
+// Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+
+var arrayPairSum = function(nums) {
+  nums.sort((a,b)=>a-b)
+  for(var out=0,i=0;i<nums.length;i+=2){
+      out+=nums[i]
+  }
+  return out
+};
