c7c343889026a5a5905340151893182f8193e0b3
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-03-08 20:00:16 -0400

    3/8/20

---
+++ codewars/findValueInBST.js
@@ -0,0 +1,6 @@
+//if you find the value in a bst, return the rest of the tree from that node
+var searchBST = function(root, val) {
+    if(root.val ==val)return root
+    let left = root.left? searchBST(root.left,val): null,right= root.right? searchBST(root.right,val):null
+    return left ? left : right ? right : null
+};
