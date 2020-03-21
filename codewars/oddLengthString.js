0861d57b94b13f4dd5cedf89285a5135ab4871ff
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-03-20 22:40:36 -0400

    ghb

--- codewars/oddLengthString.js
+++ codewars/oddLengthString.js
@@ -10,6 +10,6 @@ var generateTheString = function(n) {
     return ret
 
 };
-Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
-
-The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.  
+// Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
+//
+// The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.
