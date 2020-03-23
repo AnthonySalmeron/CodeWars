1f1ad0a4fe884e1c36fa3ce246fd8eeb40a27d9f
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-03-22 20:46:24 -0400

    af

--- codewars/distanceValue.js
+++ codewars/distanceValue.js
@@ -10,8 +10,6 @@ var findTheDistanceValue = function(arr1, arr2, d) {
     }
     return arr1.length
 };
-Given two integer arrays arr1 and arr2, and the integer d, return the distance value between the two arrays.
-
-The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
-
- 
+// Given two integer arrays arr1 and arr2, and the integer d, return the distance value between the two arrays.
+//
+// The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
