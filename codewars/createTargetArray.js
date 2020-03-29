8f5082fd4f8eb70882e1482fb1fb9389ce6b012a
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-03-29 18:35:14 -0400

    vkjhv

--- codewars/createTargetArray.js
+++ codewars/createTargetArray.js
@@ -5,12 +5,12 @@ var createTargetArray = function(nums, index) {
     }
     return target
 };
-Given two arrays of integers nums and index. Your task is to create target array under the following rules:
-
-    Initially target array is empty.
-    From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
-    Repeat the previous step until there are no elements to read in nums and index.
-
-Return the target array.
-
-It is guaranteed that the insertion operations will be valid.
+// Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+//
+//     Initially target array is empty.
+//     From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+//     Repeat the previous step until there are no elements to read in nums and index.
+//
+// Return the target array.
+//
+// It is guaranteed that the insertion operations will be valid.
