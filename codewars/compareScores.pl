f994302c1c565d44465b7bfa6074b2dca34a0682
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-04-03 20:44:06 -0400

    fjhg

--- codewars/compareScores.pl
+++ codewars/compareScores.pl
@@ -14,54 +14,6 @@ sub compareTriplets {
     return @ret;
 }
 
-# Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from to
-#
-# for three categories: problem clarity, originality, and difficulty.
-# 
-# We define the rating for Alice's challenge to be the triplet
-# , and the rating for Bob's challenge to be the triplet
-#
-# .
-#
-# Your task is to find their comparison points by comparing
-# with , with , and with
-#
-# .
-#
-#     If
-#
-# , then Alice is awarded
-# point.
-# If
-# , then Bob is awarded
-# point.
-# If
-#
-#     , then neither person receives a point.
-#
-# Comparison points is the total points a person earned.
-#
-# Given
-# and
-#
-# , determine their respective comparison points.
-#
-# For example,
-# and . For elements , Bob is awarded a point because . For the equal elements and , no points are earned. Finally, for elements , so Alice receives a point. Your return array would be
-#
-# with Alice's score first and Bob's second.
-#
-# Function Description
-#
-# Complete the function compareTriplets in the editor below. It must return an array of two integers, the first being Alice's score and the second being Bob's.
-#
-# compareTriplets has the following parameter(s):
-#
-#     a: an array of integers representing Alice's challenge rating
-#     b: an array of integers representing Bob's challenge rating
-#
-# Input Format
-#
-# The first line contains
-# space-separated integers, , , and , describing the respective values in triplet .
-# The second line contains space-separated integers, , , and , describing the respective values in triplet .
+You're given two arrays, they each contain the scores on given tests by two people. Each position in both arrays corresponds to the same tests they took.
+If student A scored higher on a test, give them a point, if student B scored higher, give them a point
+Return an array that has the form (score student A, score student B)
