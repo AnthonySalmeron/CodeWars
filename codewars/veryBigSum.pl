1edc369a782ec8cb039bf6221e1cd73c0df418d2
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-04-05 18:41:12 -0400

    4/5/20

--- codewars/veryBigSum.pl
+++ codewars/veryBigSum.pl
@@ -1,3 +1,25 @@
+#
+# Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+#
+# Function Description
+#
+# Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+#
+# aVeryBigSum has the following parameter(s):
+#
+#     ar: an array of integers .
+#
+# Input Format
+#
+# The first line of the input consists of an integer
+# .
+# The next line contains
+#
+# space-separated integers contained in the array.
+#
+# Output Format
+#
+# Print the integer sum of the elements in the array.
 sub aVeryBigSum {
     @_ = @{$_[0]};
     my @str = split(//,(pop @_));
