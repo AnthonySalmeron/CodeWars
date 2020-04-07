1e7abace1f8bb74a57134bb6110dccd5a79cf98d
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-04-06 20:40:47 -0400

    4/6/20

---
+++ codewars/diagonalDistance.pl
@@ -0,0 +1,11 @@
+sub diagonalDifference {
+    my @array= @{$_[0]};
+    my $sum1= 0;
+    my $sum2= 0;
+    for(my $var=0;$var<scalar @array;$var++){
+        my $var2 = -1*$var - 1;
+        $sum1+= $array[$var]->[$var];
+        $sum2+= $array[$var2]->[$var];
+    }
+    return abs($sum1-$sum2);
+}
