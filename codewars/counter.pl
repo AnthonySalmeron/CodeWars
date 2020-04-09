fabb609ae5ed77f4b3c2f34243f56e5a072c933f
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-04-09 18:38:20 -0400

    4/9/20

--- codewars/counter.pl
+++ codewars/counter.pl
@@ -1,3 +1,11 @@
+# Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros. Print the decimal value of each fraction on a new line.
+#
+# Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to
+#
+# are acceptable.
+#
+# For example, given the array
+# there are elements, two positive, two negative and one zero. Their ratios would be , and
 sub plusMinus {
     @_ = @{$_[0]};
     my $positives = 0;
