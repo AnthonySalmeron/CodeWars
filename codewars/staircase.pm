bdb0c73c51ae3704df99f145c4188359a5bbc21e
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-04-12 20:52:52 -0400

    gh

--- codewars/staircase.pm
+++ codewars/staircase.pm
@@ -10,17 +10,11 @@ sub staircase {
     print $str;
 }
 # Consider a staircase of size
-#
 # :
-#
 #    #
 #   ##
 #  ###
 # ####
-#
 # Observe that its base and height are both equal to
-#
 # , and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
-#
 # Write a program that prints a staircase of size
-.
