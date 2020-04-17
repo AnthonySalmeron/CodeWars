53f0353f96fbe421ef995e670ed461aee9f330f4
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-04-16 20:27:26 -0400

    4/16/20

--- codewars/birthdayCake.pm
+++ codewars/birthdayCake.pm
@@ -8,3 +8,7 @@ sub birthdayCakeCandles {
     }
     return $count;
 }
+# You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. When she blows out the candles, she’ll only be able to blow out the tallest ones. Your task is to find out how many candles she can successfully blow out.
+#
+# For example, if your niece is turning
+# years old, and the cake will have candles of height , , , , she will be able to blow out candles successfully, since the tallest candles are of height and there are such candles.
