a10227f4b37f3d47ca9057a6304fd4c7e668c1ca
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-05-06 19:37:41 -0400

    ag

--- codewars/wrongAnswerButLotsOfEffort.pm
+++ codewars/wrongAnswerButLotsOfEffort.pm
@@ -33,3 +33,8 @@ sub _helper {
         _helper($count+1 , $used.$i, $total+ $t[$i], \@t, $_[4],$_[5],$_[6]);
     }
 }
+
+# Lily has a chocolate bar that she wants to share it with Ron for his birthday. Each of the squares has an integer on it. She decides to share a contiguous segment of the bar selected such that the length of the segment matches Ron's birth month and the sum of the integers on the squares is equal to his birth day. You must determine how many ways she can divide the chocolate.
+#
+# Consider the chocolate bar as an array of squares,
+# . She wants to find segments summing to Ron's birth day, with a length equalling his birth month, . In this case, there are two segments meeting her criteria: and .
