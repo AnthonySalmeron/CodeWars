da7a5cfbfce2ca7bb4bf14f86cbc05f69667d693
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-05-09 19:03:13 -0400

    desc

--- codewars/chocolate.pm
+++ codewars/chocolate.pm
@@ -13,3 +13,4 @@ sub birthday {
     }
     return $combinations;
 }
+# Lily has a chocolate bar that she wants to share it with Ron for his birthday. Each of the squares has an integer on it. She decides to share a contiguous segment of the bar selected such that the length of the segment matches Ron's birth month and the sum of the integers on the squares is equal to his birth day. You must determine how many ways she can divide the chocolate.
