6ed6e1cdbadbc905fcc6190846255638d4593b6e
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-05-16 20:42:17 -0400

    s

--- codewars/dayOfTheProgrammer.pm
+++ codewars/dayOfTheProgrammer.pm
@@ -20,3 +20,14 @@ sub dayOfProgrammer {
         return $return1 . $year;
     }
 }
+# Marie invented a Time Machine and wants to test it by time-traveling to visit Russia on the Day of the Programmer (the day of the year) during a year in the inclusive range from to
+#
+# .
+#
+# From
+# to , Russia's official calendar was the Julian calendar; since they used the Gregorian calendar system. The transition from the Julian to Gregorian calendar system occurred in , when the next day after January was February . This means that in , February was the
+#
+# day of the year in Russia.
+#
+# In both calendar systems, February is the only month with a variable amount of days; it has
+# days during a leap year, and days during all other years. In the Julian calendar, leap years are divisible by ; in the Gregorian calendar, leap years are either of the following:
