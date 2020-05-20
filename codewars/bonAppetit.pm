0b77c6be4adbc7592ccc7aa23ee67f68e6fb2155
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-05-19 21:46:44 -0400

    s

--- codewars/bonAppetit.pm
+++ codewars/bonAppetit.pm
@@ -11,3 +11,6 @@ if($total/2 == $b){
     print ($b - $total/2);
 }
 }
+
+# Anna and Brian are sharing a meal at a restuarant and they agree to split the bill equally. Brian wants to order something that Anna is allergic to though, and they agree that Anna won't pay for that item. Brian gets the check and calculates Anna's portion. You must determine if his calculation is correct.
+# k is the index of the allergy item, b is the amount paid already
