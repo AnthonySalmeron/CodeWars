96a135ed42f85e117b33cb98bf5b59fb30923f23
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-05-21 20:56:12 -0400

    s

--- codewars/merchantPairs.pm
+++ codewars/merchantPairs.pm
@@ -15,3 +15,4 @@ sub sockMerchant {
     }
     return $pairs;
 }
+# John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
