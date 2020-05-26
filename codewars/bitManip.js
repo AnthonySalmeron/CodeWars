47b0ea49f71a05ba7d12cc8a09e1c7953a4c3a35
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-05-25 21:41:55 -0400

    a

--- codewars/bitManip.js
+++ codewars/bitManip.js
@@ -5,10 +5,10 @@ function killKthBit(n, k) {
 function killKthBit(n, k) {
   return n & ~(1<<k-1);
 }
-& = sets a bit to 1 if both are 1
-~ = not, switches all bits
-<< = left shift, adds certain amount of 0 bits to the right
+// & = sets a bit to 1 if both are 1
+// ~ = not, switches all bits
+// << = left shift, adds certain amount of 0 bits to the right
 
-In order to stop the Mad Coder evil genius you need to decipher the encrypted message he sent to his minions. The message contains several numbers that, when typed into a supercomputer, will launch a missile into the sky blocking out the sun, and making all the people on Earth grumpy and sad.
-
-You figured out that some numbers have a modified single digit in their binary representation. More specifically, in the given number n the kth bit from the right was initially set to 0, but its current value might be different. It's now up to you to write a function that will change the kth bit of n back to 0.
+// In order to stop the Mad Coder evil genius you need to decipher the encrypted message he sent to his minions. The message contains several numbers that, when typed into a supercomputer, will launch a missile into the sky blocking out the sun, and making all the people on Earth grumpy and sad.
+//
+// You figured out that some numbers have a modified single digit in their binary representation. More specifically, in the given number n the kth bit from the right was initially set to 0, but its current value might be different. It's now up to you to write a function that will change the kth bit of n back to 0.
