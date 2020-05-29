ec48c55daa6707718611ed7efe293909de0ff9c7
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-05-28 21:38:04 -0400

    5/28/20

--- codewars/killBit.js
+++ codewars/killBit.js
@@ -17,3 +17,11 @@ sub arrayPacking {
 	}
 	return $ret;
 }
+You are given an array of up to four non-negative integers, each less than 256.
+
+Your task is to pack these integers into one number M in the following way:
+
+    The first element of the array occupies the first 8 bits of M;
+    The second element occupies next 8 bits, and so on.
+
+Return the obtained integer M.
