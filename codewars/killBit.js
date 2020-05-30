68ff2821741a79bcf1d75cf2ee34fe4b1ff1cdfd
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-05-29 23:02:32 -0400

    n

--- codewars/killBit.js
+++ codewars/killBit.js
@@ -9,19 +9,19 @@ let helper = e => {
 }
 
 //different perl solution
-sub arrayPacking {
-	my ($a) = @_;
-	my $ret = 0;
-	for(my $i=0;$i<scalar @$a;$i++){
-		$ret+= $a->[$i] << 8*$i;
-	}
-	return $ret;
-}
-You are given an array of up to four non-negative integers, each less than 256.
-
-Your task is to pack these integers into one number M in the following way:
-
-    The first element of the array occupies the first 8 bits of M;
-    The second element occupies next 8 bits, and so on.
-
-Return the obtained integer M.
+// sub arrayPacking {
+// 	my ($a) = @_;
+// 	my $ret = 0;
+// 	for(my $i=0;$i<scalar @$a;$i++){
+// 		$ret+= $a->[$i] << 8*$i;
+// 	}
+// 	return $ret;
+// }
+// You are given an array of up to four non-negative integers, each less than 256.
+//
+// Your task is to pack these integers into one number M in the following way:
+//
+//     The first element of the array occupies the first 8 bits of M;
+//     The second element occupies next 8 bits, and so on.
+//
+// Return the obtained integer M.
