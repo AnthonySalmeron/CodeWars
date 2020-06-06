8b62420df3a71ec23d34c539547a3cc21048704d
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-06-05 20:59:16 -0400

    hard

--- codewars/rangeBitCounter.js
+++ codewars/rangeBitCounter.js
@@ -3,4 +3,4 @@ function rangeBitCount(a, b) {
 }
 // 1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,1
 //definitely a pattern with how many times they show up
-// distance between 1's is 2X+1
+// distance between 1's is 2X+1 where x is the number of 1's so far
