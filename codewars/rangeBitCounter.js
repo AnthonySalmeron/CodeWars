242b077136ad9df06880573d74b47031e5449318
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-06-06 19:56:13 -0400

    finish

--- codewars/rangeBitCounter.js
+++ codewars/rangeBitCounter.js
@@ -1,6 +1,14 @@
-function rangeBitCount(a, b) {
+// let rangeBitCount = (a, b) => Array.from({ length: (b - a) /1 + 1}, (_, i) => a + i).reduce((sum,e)=> sum + e.toString(2).match(/1/g).length);
+let rangeBitCount = (a,b) => {
+    let array = Array.from({ length: (b - a) /1 + 1}, (_, i) => a + i)
+    array.forEach((e,i)=>{
+        array[i] = e.toString(2)
+    })
+    array.forEach((e,i)=> array[i]= e.match(/1/g) ? e.match(/1/g).length : 0)
+    return array.reduce((sum,curr)=> sum + curr)
 
 }
+// 1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,1
 // 1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,1
 //definitely a pattern with how many times they show up
 // distance between 1's is 2X+1 where x is the number of 1's so far
