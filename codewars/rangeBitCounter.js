a2495310a727011b73188c4c6896d7f1f31ab4bd
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-06-07 21:21:48 -0400

    perl solution

--- codewars/rangeBitCounter.js
+++ codewars/rangeBitCounter.js
@@ -12,3 +12,16 @@ let rangeBitCount = (a,b) => {
 // 1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,1
 //definitely a pattern with how many times they show up
 // distance between 1's is 2X+1 where x is the number of 1's so far
+
+// perl
+sub rangeBitCount{
+	my ($a, $b) = @_;
+    my $res = 0;
+    foreach ($a..$b){
+        while ($_ > 0) {
+            $res += $_ & 1;
+            $_ = ($_ >> 1);
+        }
+    }
+    return $res;
+}
