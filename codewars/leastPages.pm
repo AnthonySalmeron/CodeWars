22f9b5458a00116f361a9bf5e8d327267287353f
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-06-18 19:15:16 -0400

    n

--- codewars/leastPages.pm
+++ codewars/leastPages.pm
@@ -5,3 +5,9 @@ sub pageCount {
     my $right = int(($n-$p)/2) + ((($n % 2 == 0)&&($p % 2 != 0)) ? 1 : 0);
     return $right<$left ? $right : $left;
 }
+# Complete the pageCount function in the editor below. It should return the minimum number of pages Brie must turn.
+#
+# pageCount has the following parameter(s):
+#
+#     n: the number of pages in the book
+#     p: the page number to turn to
