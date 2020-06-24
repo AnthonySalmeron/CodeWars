b22e064b69ee5d83409eb52f023c6e11adbf6c11
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-06-23 21:04:49 -0400

    desc

--- codewars/sumOfTwo.js
+++ codewars/sumOfTwo.js
@@ -4,3 +4,5 @@ function sumOfTwo(a, b, v) {
     b.forEach(e => obj[e] ?  t = true : '')
     return t
 }
+
+// You have two integer arrays, a and b, and an integer target value v. Determine whether there is a pair of numbers, where one number is taken from a and the other from b, that can be added together to get a sum of v. Return true if such a pair exists, otherwise return false.
