bf123521fc13aeb2c6a1fc22a3e96a5417675d8d
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-06-28 20:06:31 -0400

    sdf

--- codewars/sumOfTwo.js
+++ codewars/sumOfTwo.js
@@ -6,6 +6,8 @@ function sumOfTwo(a, b, v) {
 }
 
 // You have two integer arrays, a and b, and an integer target value v. Determine whether there is a pair of numbers, where one number is taken from a and the other from b, that can be added together to get a sum of v. Return true if such a pair exists, otherwise return false.
+
+
 // perl
 // sub sumOfTwo {
 // 	my ($a, $b, $v) = @_;
