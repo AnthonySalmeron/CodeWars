83fb1d5052c9f0bb1b5f09c9d3a727d6e96903ac
asalmeron <asalmeron@athenahealth.com>
2020-08-27 19:21:04 -0400

    8/27/20

--- codewars/reorganizeString.js
+++ codewars/reorganizeString.js
@@ -1,11 +1,23 @@
 var reorganizeString = function(s) {
     //First thing I noticed is that if there is any character in the string which appears > S.length/2 times, then it's an immediate fail 
     if (s.length === 1) return ""
+    const maxLen = Math.ceil(s.length/2)
     
     let charCounts = {}
+    let degreeOfChars = {1:{}}
+    let highestCount = 1
     for(let letter of s){
         if(charCounts[letter]) charCounts[letter]++
         else charCounts[letter] = 1
+        
+        if (charCounts[letter] > maxLen) return ""
+        
+        if(charCounts[letter]>1){
+            if (charCounts[letter]>highestCount) highestCount = charCounts[letter]
+            
+            degreeOfChars[charCounts[letter]][letter] = 1
+            delete degreeOfChars[charCounts[letter]-1][letter]
+        }
     }
     
     //possibly have another object that has the form {numberOfChar: [array of chars with this number]} and update this every time a letter is used to add to the string so every time only the highest number chars are used
