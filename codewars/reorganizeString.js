2524559ca8d2c474222adc27797a9631c2bc575c
asalmeron <asalmeron@athenahealth.com>
2020-09-04 22:01:11 -0400

    spaces

--- codewars/reorganizeString.js
+++ codewars/reorganizeString.js
@@ -2,21 +2,21 @@ var reorganizeString = function(s) {
     //First thing I noticed is that if there is any character in the string which appears > S.length/2 times, then it's an immediate fail 
     if (s.length === 1) return ""
     const maxLen = Math.ceil(s.length/2)
-    
+
     let charCounts = {}
     let degreeOfChars = {1:{}}
     let highestCount = 1
     for(let letter of s){
         if(charCounts[letter]) charCounts[letter]++
         else charCounts[letter] = 1
-        
+
         if (charCounts[letter] > maxLen) return ""
-        
+
         degreeOfChars[1][letter] = 1
-        
+
         if(charCounts[letter]>1){
             if (charCounts[letter]>highestCount) highestCount = charCounts[letter]
-            
+
             if(!degreeOfChars[charCounts[letter]]) degreeOfChars[charCounts[letter]] = {}
             degreeOfChars[charCounts[letter]][letter] = 1
             letter === 2 ?
@@ -24,7 +24,7 @@ var reorganizeString = function(s) {
                 delete degreeOfChars[charCounts[letter]-1][letter]
         }
     }
-    
+
     let output = ""
     while(output.length<s.length){
         console.log(degreeOfChars)
